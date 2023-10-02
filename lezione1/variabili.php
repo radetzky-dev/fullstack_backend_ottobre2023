87271dd9e3f3bd449030b30732eb8bfca2a92e3b
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-02 13:58:57 +0200

    vars

--- lezione1/variabili.php
+++ lezione1/variabili.php
@@ -6,8 +6,26 @@ $d = 1;
 
 function sum($a, $b)
 {
+    $myName ="mario";
     global $c;
+    echo "Ciao sono $myName";
 
     return $a + $b + $c + $GLOBALS['d'];
 }
+
+function testStatic()
+{
+    static $num =10;
+    echo $num;
+    $num++;
+}
+
 echo sum($a, 10);
+echo "<hr>";
+testStatic();
+echo "<hr>";
+testStatic();
+echo "<hr>";
+testStatic();
+echo "<hr>";
+testStatic();

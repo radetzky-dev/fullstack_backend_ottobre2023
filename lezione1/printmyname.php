6ca61766e70864b85bd9f562bbb7de44f8718188
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-02 12:55:22 +0200

    my name

--- lezione1/printmyname.php
+++ lezione1/printmyname.php
@@ -12,5 +12,19 @@ commenti
 
 $name ="Paolo";
 
+printMyName($name);
+printMyName("Maria");
+
 // Questo stampa il mio nome
 echo "My name is $name";
+
+
+/**
+ * print_my_name
+ * funzione per stampare il nome
+ * @param  mixed $name
+ * @return void
+ */
+function printMyName($name) {
+    echo "<strong>Hello " . $name . "!</strong><br>";
+}

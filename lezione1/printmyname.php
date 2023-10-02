482479807338dcbe93d2b21d4cc8388ec72fb787
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-02 12:49:16 +0200

    commenti

---
+++ lezione1/printmyname.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+This is a comment multi
+lines
+commenti
+*/
+
+// this is a single line comment
+
+# another comment type - meno usata
+
+$name ="Paolo";
+
+// Questo stampa il mio nome
+echo "My name is $name";

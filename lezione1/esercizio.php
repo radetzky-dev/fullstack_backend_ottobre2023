1ab36cb52b10e2dfec1b085326603fbad16fd5f5
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-02 14:12:19 +0200

    esercizio

---
+++ lezione1/esercizio.php
@@ -0,0 +1,26 @@
+<?php
+$siteTitle = "Benvenuto nella mia pagina";
+$name = "Paolo";
+$myName = "dhjsdf";
+?>
+<!DOCTYPE html>
+<html lang="ita">
+
+<head>
+    <title><?php echo $siteTitle . ' ' . $name; ?></title>
+</head>
+<body>
+<p>
+        TIME: <?php echo date('H:i'); ?> <br>DAY <?php echo date('d/m/Y'); ?>
+
+        <table>
+            <tr><td><img src="assets/dolce1.jpg" alt="image"></td><td></td><td></td></tr>
+</table>
+    </p>
+    <script>
+        my_name="<?php echo $name; ?>";
+        alert("Ciao "+my_name);
+    </script>
+
+</body>
+</html>

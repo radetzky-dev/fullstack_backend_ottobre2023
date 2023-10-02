b101662587ae41d6e7c3ab6c0bfdeae25787f0e0
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-02 10:57:14 +0200

    test

--- lezione1/helloworld.php
+++ lezione1/helloworld.php
@@ -1,5 +1,8 @@
+
+<h2>Questo è un titolo</h2>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A itaque reiciendis exercitationem animi, sed ut libero id perspiciatis, ipsam, excepturi fuga quas incidunt? Nemo, laboriosam corrupti corporis dignissimos quas enim!</p>
 <?php
 $test ="prova";
 
-echo "Hello $test world\n\n";
-echo 'ciao '.$test.' mondo';
+echo "<p>Hello $test world</p>";
+echo '<p>ciao '.$test.' mondo</p>';

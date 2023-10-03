d3575df46c1defbd454f197b3916c30f37a892ad
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-03 09:54:37 +0200

    auto

---
+++ lezione2/array.php
@@ -0,0 +1,12 @@
+<?php
+
+//inizia da 0
+$cars = array ("Volvo", "Ferrari", "Fiat","Saab");
+
+//stampa il contenuto dell'array
+
+
+echo "La tua auto è una ". $cars[1]."<br>";
+echo "La tua auto è una ". $cars[3]."<br>";
+
+var_dump ($cars);

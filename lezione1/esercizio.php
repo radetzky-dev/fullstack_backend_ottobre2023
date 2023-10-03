1cff0ef996a3c0d52098a10dbac073bb474c89e7
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-03 09:47:09 +0200

    aggiornamento del titolo

--- lezione1/esercizio.php
+++ lezione1/esercizio.php
@@ -10,10 +10,10 @@ $myName = "dhjsdf";
     <title><?php echo $siteTitle . ' ' . $name; ?></title>
 </head>
 <body>
+    <h2>Titolo</h2>
 <p>
         TIME: <?php echo date('H:i'); ?> <br>DAY <?php echo date('d/m/Y'); ?>
-
-        <table>
+<table>
             <tr><td><img src="assets/dolce1.jpg" alt="image"></td><td></td><td></td></tr>
 </table>
     </p>

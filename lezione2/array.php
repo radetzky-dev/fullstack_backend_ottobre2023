32727ab6cb4971bcdf99b4648e6598f218fc8390
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-03 10:03:44 +0200

    array

--- lezione2/array.php
+++ lezione2/array.php
@@ -1,12 +1,21 @@
 <?php
 
 //inizia da 0
-$cars = array ("Volvo", "Ferrari", "Fiat","Saab");
+$myVar = "Skoda";
+
+$cars = array ("Volvo", "Ferrari", "Fiat","Saab",12, true,33.4, $myVar);
 
 //stampa il contenuto dell'array
 
-
 echo "La tua auto è una ". $cars[1]."<br>";
 echo "La tua auto è una ". $cars[3]."<br>";
+echo "La tua auto è una ". $cars[7]."<br>";
 
 var_dump ($cars);
+
+echo "<hr>";
+$array = array(1, 2, 3, 4,5, 8 => "mario",  4 => "quarta", 19, 3 => 13);
+print_r($array);
+
+echo "Stampa la posizione 4 ".$array[4]."<br>";
+echo "Stampa la posizione 3 ".$array[3]."<br>";

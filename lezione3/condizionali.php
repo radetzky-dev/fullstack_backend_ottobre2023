4abed4c7867e5fad7eac239773044028e0aeb2d6
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 09:53:20 +0200

    condizionali

--- lezione3/condizionali.php
+++ lezione3/condizionali.php
@@ -1,12 +1,30 @@
 <?php
 
 //ternary condition
-$marks=399;
-echo ($marks>=40) ? "pass" : "Fail";
+$marks = 399;
+echo ($marks >= 40) ? "pass" : "Fail";
 
 echo "<hr>";
-$is_user_logged_in = false;
+$is_user_logged_in = true;
 
 $title = $is_user_logged_in ? 'Logout' : 'Login';
 
 echo "Action: $title";
+
+echo "<hr>";
+//if
+$a = 7;
+$b = 5;
+$c = "7";
+
+if ($a > $b) {
+    echo "a is bigger than b<br>";
+}
+
+if ($a == $c) {
+    echo "Uguali ma non identici<br>";
+}
+
+if ($is_user_logged_in) {
+    echo "Utente loggato<br>";
+}

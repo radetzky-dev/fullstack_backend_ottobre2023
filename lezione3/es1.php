76bce05d9c7caddf5d12601bfbc2e5026d3d5355
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 10:31:11 +0200

    legalage

--- lezione3/es1.php
+++ lezione3/es1.php
@@ -1,3 +1,22 @@
 <?php
-
+$legalAge = 21;
+$name ="Mario";
+$age =12;
 //check se una persona può votare in base all'età
+
+/*
+if ($age >= $legaleAge) {
+    echo $name . ", puoi votare";
+} else {
+    echo $name . ", non puoi votare. ";
+}
+
+*/
+function checkAge($age, $name, $legalAge = 18)
+{
+    echo "Per votare occorrone $legalAge anni, tu ne hai $age<br>";
+    echo ($age >= $legalAge) ?  "$name, puoi votare" : "$name, non puoi votare.";
+    echo "<br>";
+}
+
+checkAge($age,$name);

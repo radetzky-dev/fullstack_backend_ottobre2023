52704e97ee42b39bd22c7f78375a3f9f37669644
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 12:02:01 +0200

    esercizio

--- lezione3/es1.php
+++ lezione3/es1.php
@@ -1,17 +1,44 @@
 <?php
-$legalAge = 21;
-$name ="Mario";
-$age =12;
+$name = "Mario";
+$age = 12;
 //check se una persona può votare in base all'età
 
-function checkAge($age, $name, $legalAge = 18)
+/**
+ * checkAge
+ *
+ * @param  mixed $age
+ * @param  mixed $name
+ * @param  mixed $legalAge
+ * @return void
+ */
+function checkAge(int $age, string $name, int $legalAge = 18): void
 {
-    echo "Per votare occorrone $legalAge anni, tu ne hai $age<br>";
-    echo ($age >= $legalAge) ?  "$name, puoi votare" : "$name, non puoi votare.";
+    echo "Per votare occorrono $legalAge anni, tu ne hai $age<br>";
+    echo ($age >= $legalAge) ? "$name, puoi votare" : "$name, <b>non</b> puoi votare.";
     echo "<br>";
 }
 
-//array con le informazioni nome e age per almeno 3 persone e con un ciclo testiamo
-//con la funzione
+$people = array(
+    "Mario" => 25,
+    "Piero" => 21,
+    "Marisa" => 32,
+    "Sonia" => 5,
+);
 
-checkAge($age,$name);
+foreach ($people as $name => $age) {
+    checkAge($age, $name);
+}
+
+$people = array(
+    array("mario", 21),
+    array("anna", 15),
+    array("gianni", 6),
+);
+
+for ($i = 0; $i < count($people); $i++) {
+    checkAge($people[$i][1], $people[$i][0]);
+}
+
+foreach ($people as $singlePerson) {
+    checkAge($singlePerson[1], $singlePerson[0]);
+}

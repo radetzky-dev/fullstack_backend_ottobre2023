452be508287341c48b505c3aeea0ed715669e401
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 10:43:31 +0200

    esercizio

--- lezione3/es1.php
+++ lezione3/es1.php
@@ -4,19 +4,14 @@ $name ="Mario";
 $age =12;
 //check se una persona può votare in base all'età
 
-/*
-if ($age >= $legaleAge) {
-    echo $name . ", puoi votare";
-} else {
-    echo $name . ", non puoi votare. ";
-}
-
-*/
 function checkAge($age, $name, $legalAge = 18)
 {
     echo "Per votare occorrone $legalAge anni, tu ne hai $age<br>";
     echo ($age >= $legalAge) ?  "$name, puoi votare" : "$name, non puoi votare.";
     echo "<br>";
 }
+
+//array con le informazioni nome e age per almeno 3 persone e con un ciclo testiamo
+//con la funzione
 
 checkAge($age,$name);

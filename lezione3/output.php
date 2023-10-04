4202a9ad4835379905a3790b7b1e23ac75299c19
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 12:48:01 +0200

    pre

--- lezione3/output.php
+++ lezione3/output.php
@@ -9,6 +9,10 @@ $a = array("a" => "apple", "b" => "banana", "z" => "pesca", 3, true);
 
 var_dump($a);
 
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+
 ?>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur molestiae inventore corrupti hic. Dolorem, dolorum, obcaecati maiores natus odit hic neque voluptatum architecto fugit enim asperiores quibusdam? Delectus, reiciendis tenetur.</p>
@@ -18,5 +22,6 @@ var_dump($a);
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus esse quae, ipsum veritatis at praesentium dicta provident eum blanditiis quod earum dignissimos deleniti odit? Aliquam modi necessitatibus voluptatem perspiciatis nihil.</p>
 
 <?php
-
+echo "<pre>";
 print_r($a);
+echo "</pre>";

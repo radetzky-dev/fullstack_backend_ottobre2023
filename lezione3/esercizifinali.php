76930773ecc129e8606f8e58d10e4ac6c44a84ad
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 14:25:03 +0200

    soluzione

--- lezione3/esercizifinali.php
+++ lezione3/esercizifinali.php
@@ -2,7 +2,7 @@
 
 /*
 1)
- $color = array('white', 'green', 'red'')
+$color = array('white', 'green', 'red');
 
 Output :
 
@@ -10,11 +10,36 @@ green
 red
 white
 
-
-
 2)
 $x = array(1, 2, 3, 4, 5);
-Delete an element from the above PHP array. After deleting the element, integer keys must be normalized. 
-array(5) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(4) [4]=> int(5) } 
+Delete an element from the above PHP array. After deleting the element, integer keys must be normalized.
+array(5) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(4) [4]=> int(5) }
 array(4) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(5) }
-*/
+ */
+
+$color = array('white', 'green', 'red');
+sort($color);
+echo "<ul>";
+foreach ($color as $y) {
+    echo "<li>$y</li>";
+}
+echo "</ul>";
+
+$x = array(1, 2, 3, 4, 5);
+var_dump($x);
+unset($x[3]);
+$x = array_values($x);
+echo '<br>';
+var_dump($x);
+
+echo '<br>';
+$y = array(1, 2, "re", "pa", 5);
+echo "<pre>";
+print_r($y);
+echo "</pre>";
+unset($y[1]);
+$y = array_values($y);
+echo '<br>';
+echo "<pre>";
+print_r($y);
+echo "</pre>";

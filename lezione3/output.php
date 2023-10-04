ece63b5b2e677eaaceafdfadd1a788e6e3f75cdc
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 09:38:13 +0200

    output

---
+++ lezione3/output.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Hello world!";
+
+$myVar="prova";
+?>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur molestiae inventore corrupti hic. Dolorem, dolorum, obcaecati maiores natus odit hic neque voluptatum architecto fugit enim asperiores quibusdam? Delectus, reiciendis tenetur.</p>
+
+<?=$myVar;?>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus esse quae, ipsum veritatis at praesentium dicta provident eum blanditiis quod earum dignissimos deleniti odit? Aliquam modi necessitatibus voluptatem perspiciatis nihil.</p>

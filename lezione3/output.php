1ff64dc5c55f9204b7897b17dad1b2337d8b3563
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 09:42:46 +0200

    print

--- lezione3/output.php
+++ lezione3/output.php
@@ -1,8 +1,14 @@
 <?php
 
-echo "Hello world!";
+print "ciao<br>"."ddd<br>";
+echo "Hello world!<br>";
 
 $myVar="prova";
+
+$a = array("a" => "apple", "b" => "banana", "z" => "pesca", 3, true);
+
+var_dump($a);
+
 ?>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur molestiae inventore corrupti hic. Dolorem, dolorum, obcaecati maiores natus odit hic neque voluptatum architecto fugit enim asperiores quibusdam? Delectus, reiciendis tenetur.</p>
@@ -10,3 +16,7 @@ $myVar="prova";
 <?=$myVar;?>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus esse quae, ipsum veritatis at praesentium dicta provident eum blanditiis quod earum dignissimos deleniti odit? Aliquam modi necessitatibus voluptatem perspiciatis nihil.</p>
+
+<?php
+
+print_r($a);

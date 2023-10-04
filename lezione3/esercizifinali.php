09443f41e024b7b751c263de8ef4b532bfd1ce28
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-04 14:04:43 +0200

    esercizi

---
+++ lezione3/esercizifinali.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+1)
+ $color = array('white', 'green', 'red'')
+
+Output :
+
+green
+red
+white
+
+
+
+2)
+$x = array(1, 2, 3, 4, 5);
+Delete an element from the above PHP array. After deleting the element, integer keys must be normalized. 
+array(5) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(4) [4]=> int(5) } 
+array(4) { [0]=> int(1) [1]=> int(2) [2]=> int(3) [3]=> int(5) }
+*/

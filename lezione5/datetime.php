432fff89c9bb3a123b1e1398bfa424cc0bb9840f
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-06 10:10:33 +0200

    datetime

--- lezione5/datetime.php
+++ lezione5/datetime.php
@@ -1,6 +1,7 @@
 <?php
+
 echo "In secondi dal 1970<br>";
-echo 'Now: '. time();
+echo 'Now: ' . time();
 
 echo strtotime("now"), "<br>";
 echo strtotime("10 September 2000"), "<br>";
@@ -12,28 +13,27 @@ echo strtotime("last Monday"), "<br>";
 
 echo "<hr>";
 
-
 echo date('l jS \of F Y h:i:s A');
 echo "<hr>";
 echo date("l");
 echo "<hr>";
 echo "July 1, 2000 is on a " . date("l", mktime(0, 0, 0, 7, 1, 2000));
 echo "<hr>";
-$tomorrow  = date("l j F Y",mktime(0, 0, 0, date("m")  , date("d")+1, date("Y")));
-echo "DOMANI ".$tomorrow."<br>";
-$nextyear  = mktime(0, 0, 0, date("m"),   date("d"),   date("Y")+1);
-echo $nextyear."<br>";
+$tomorrow = date("l j F Y", mktime(0, 0, 0, date("m"), date("d") + 1, date("Y")));
+echo "DOMANI " . $tomorrow . "<br>";
+$nextyear = mktime(0, 0, 0, date("m"), date("d"), date("Y") + 1);
+echo $nextyear . "<br>";
 echo "<hr>";
-$today = date("F j, Y, g:i a");                 // March 10, 2001, 5:16 pm
-$today = date("d.m.y");                         // 03.10.01
-echo "Oggi in formato italiano ".$today."<br>";
-$today = date("j, n, Y");                       // 10, 3, 2001
-$today = date("dmY");                           // 20010310
-echo "Oggi in formato italiano ".$today."<br>";
-$today = date("d-m-Y H:i");                   // 2001-03-10 17:16:18 (the MySQL DATETIME format)
-echo "Oggi in formato italiano ".$today."<br>";
+$today = date("F j, Y, g:i a"); // March 10, 2001, 5:16 pm
+$today = date("d.m.y"); // 03.10.01
+echo "Oggi in formato italiano " . $today . "<br>";
+$today = date("j, n, Y"); // 10, 3, 2001
+$today = date("dmY"); // 20010310
+echo "Oggi in formato italiano " . $today . "<br>";
+$today = date("d-m-Y H:i"); // 2001-03-10 17:16:18 (the MySQL DATETIME format)
+echo "Oggi in formato italiano " . $today . "<br>";
 $ora = date("H:i");
-echo "Sono le ".$ora."<br>";
+echo "Sono le " . $ora . "<br>";
 echo "Ottobre 6, 2023 is on a " . date("l", mktime(0, 0, 0, 10, 6, 2023));
 echo "<hr>";
 $today = getdate();
@@ -41,11 +41,39 @@ echo "<pre>";
 print_r($today);
 echo "</pre>";
 
-echo $today['mday']."-".$today['mon']."-".$today['year']."<br>";
-echo "<hr>";
-echo "<hr>";
-echo "<hr>";
+echo $today['mday'] . "-" . $today['mon'] . "-" . $today['year'] . "<br>";
 echo "<hr>";
 
+$italian_timezone = new DateTimeZone("Europe/Rome");
+$utc_timezone = new DateTimeZone("UTC");
+$tallinn_timezone = new DateTimeZone("Europe/Tallinn");
+// Create a new DateTime object in the UTC format
+
+$datetime = new DateTime("2023-01-01 11:00:00", $utc_timezone);
+// Convert the DateTime object to the timezone of Tallinn
+$datetime->setTimezone($tallinn_timezone);
+// Display the result in the YYYY-MM-DD HH:MM:SS format
+echo "TALLIN " . $datetime->format('Y-m-d H:i:s') . '<br>';
+
+// Convert the DateTime object to the timezone of Tallinn
+$datetime->setTimezone($italian_timezone);
+// Display the result in the YYYY-MM-DD HH:MM:SS format
+echo "ROMA " . $datetime->format('Y-m-d H:i:s') . '<br>';
+
+echo "<hr>";
+
+echo "DATETIME<br>";
+$date = new DateTime();
+$tomorrow = $date->modify('+1 day');
+echo "OGGI ". $date->format('Y-m-d');
+echo " DOMANI ". $tomorrow->format('Y-m-d');
+
+echo "<br>DATETIMEIMMUTABLE<br>";
+$date = new DateTimeImmutable();
+$tomorrow = $date->modify('+1 day');
+echo "OGGI ".$date->format('Y-m-d');
+echo " DOMANI ".$tomorrow->format('Y-m-d');
 
 
+echo "<hr>";
+echo "<hr>";

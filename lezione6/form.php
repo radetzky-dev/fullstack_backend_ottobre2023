30a3fa1852bb89998e1175f6adff22ebae891317
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-09 11:33:42 +0200

    dummy

--- lezione6/form.php
+++ lezione6/form.php
@@ -9,16 +9,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <?php
+$dummyPhoto = "assets/dummyphoto.png";
+//$dummyPhoto = "https://dummyimage.com/300";
 
-/*
-creare un form con 3 campi, nome e
-cognome e numero di telefono..
-- foto del contatto
-- ripopolare il form dell’esempio di codice con
-la select, o con i checkbox.
- */
-
-$name = $surname = $phone = "";
+$name = $surname = $phone = $anagraficaArray =$company = $qualifica = $dummyName= $email =$birthdate = $dummySurname = $dummyText = "";
 
 ?>
 
@@ -26,6 +20,22 @@ $name = $surname = $phone = "";
     <div class="container">
         <h3>Registration form</h3>
         <div class="row">
+        <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="<?= $dummyPhoto; ?>" id="profilePhoto" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title" id="namesurname"><?= $dummyName . ' ' . $dummySurname; ?></h5>
+                        <p class="card-text"><?= $dummyText; ?></p>
+                        <button class="btn btn-primary">Dati corretti: registrami</button>
+                    </div>
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" id="FormProfilePhoto">
+                        <input type="file" name="file">
+                        <input type="hidden" name="uploadPhoto" value="uploadPhoto" />
+                        <input type="hidden" name="otherFormInfo" value="<?= $anagraficaArray; ?>" />
+                        <input type="submit" name="upload" value="Carica foto profilo">
+                    </form>
+                </div>
+            </div>
 
             <div class="col">
                 <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
@@ -40,10 +50,30 @@ $name = $surname = $phone = "";
                             value="<?= $surname ?>" required>
                     </div>
                     <div class="form-group">
+                        <label for="company">Società</label>
+                        <input type="text" class="form-control" id="societa" name="societa" placeholder="Società (opz)" value="<?= $company ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="company">Qualifica</label>
+                        <input type="text" class="form-control" id="qualifica" name="qualifica" placeholder="Qualifica (opz)" value="<?= $qualifica ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email (opz)" value="<?= $email ?>" required>
+                    </div>
+                    <div class="form-group">
                         <label for="exampleInputTelephone">Telefono</label>
                         <input type="number" class="form-control" id="telefono" name="telefono" placeholder="Telefono"
                             value="<?= $phone ?>" required>
                     </div>
+                    <div class="form-group">
+                        <label for="exampleInputBirthDate">Data di nascita</label>
+                        <input type="date" class="form-control" id="birthdate" name="birthdate" placeholder="Data di nascita"
+                            value="<?= $birthdate ?>" required>
+                    </div>
+
+                
+
                     <div class="form-group form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
                         <label class="form-check-label" for="exampleCheck1">Accetta i nostri termini di servizio</label>
@@ -53,3 +83,7 @@ $name = $surname = $phone = "";
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>

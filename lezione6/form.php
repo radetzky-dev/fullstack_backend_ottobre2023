82bcb1b0c00958b1a4c40e1094a51c08ab836f24
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-09 10:48:30 +0200

    esercizio

---
+++ lezione6/form.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+creare un form con 3 campi, nome e
+cognome e numero di telefono..
+- foto del contatto
+- ripopolare il form dell’esempio di codice con
+la select, o con i checkbox.
+*/
+  

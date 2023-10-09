5314c9cab6657898a105690084b19c620dff23ba
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-09 11:09:11 +0200

    form

--- lezione6/form.php
+++ lezione6/form.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
 <?php
 
 /*
@@ -6,5 +16,40 @@ cognome e numero di telefono..
 - foto del contatto
 - ripopolare il form dell’esempio di codice con
 la select, o con i checkbox.
-*/
-  
+ */
+
+$name = $surname = $phone = "";
+
+?>
+
+<body>
+    <div class="container">
+        <h3>Registration form</h3>
+        <div class="row">
+
+            <div class="col">
+                <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome"
+                            value="<?= $name ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="cognome">Cognome</label>
+                        <input type="text" class="form-control" id="cognome" name="cognome" placeholder="Cognome"
+                            value="<?= $surname ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputTelephone">Telefono</label>
+                        <input type="number" class="form-control" id="telefono" name="telefono" placeholder="Telefono"
+                            value="<?= $phone ?>" required>
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
+                        <label class="form-check-label" for="exampleCheck1">Accetta i nostri termini di servizio</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>

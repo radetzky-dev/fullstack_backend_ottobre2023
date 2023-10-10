131f5682133485653e80f691a2b91ed8eb398903
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-10 10:10:12 +0200

    esercizio

--- lezione7/filesop.php
+++ lezione7/filesop.php
@@ -2,41 +2,63 @@
 
 <head>
     <title>Files</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
 </head>
 
 <body>
+
+    <div class="container">
     <h2>Scrittura di un file</h2>
+        <div class="row">
+            <div class="col">
+                <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <div class="form-group">
+                        <label for="testo">Testo da salvare nel file</label>
+                        <input type="text" class="form-control" id="testo" name="testo" placeholder="Testo" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
     <?php
+
+    /**
+     * writeContentIntoFile
+     *
+     * @param  mixed $fileNameWithPath
+     * @param  mixed $content
+     * @return bool
+     */
+    function writeContentIntoFile($fileNameWithPath, $content): bool
+    {
+        if (!$fp = fopen($fileNameWithPath, 'wb')) {
+            return false;
+        }
+        if (fwrite($fp, $content) === false) {
+            return false;
+        }
+        fclose($fp);
+        return true;
+    }
+
+    //params
     $PATH_DIR = "data/";
     $somecontent = "Add this xxxx to the file\n";
     $filename = 'numbers.dat';
 
+    //programma
+    $result = writeContentIntoFile($PATH_DIR . $filename, $somecontent);
 
-    // Let's make sure the file exists and is writable first.
-    if (is_writable($PATH_DIR . $filename)) {
-        if (!$fp = fopen($PATH_DIR . $filename, 'wb')) {
-            echo "Cannot open file ($filename)";
-            exit;
-        }
-
-        // Write $somecontent to our opened file.
-        if (fwrite($fp, $somecontent) === false) {
-            echo "Cannot write to file ($filename)";
-            exit;
-        }
-        echo "Success, wrote ($somecontent) to file ($filename)";
-        fclose($fp);
-
+    if (!$result) {
+        echo "Errore nella scrittura \ lettura del file: $PATH_DIR$filename!<br>";
     } else {
-        echo "The file $filename is not writable on not exists.";
-        $f = fopen($PATH_DIR.$filename, 'wb');
-        if (!$f) {
-            die('Error creating the file ' . $PATH_DIR.$filename);
-        }
-        echo "File created successfully $filename";
-        fclose($f);
+        echo "Success, wrote ($somecontent) to file ($PATH_DIR$filename)";
     }
+
     ?>
 </body>
 

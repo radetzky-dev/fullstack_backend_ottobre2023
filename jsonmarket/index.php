8a4e5180ba7f95f85dced0e789e53bd1da3a2a4f
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-18 13:40:19 +0200

    carrello

--- jsonmarket/index.php
+++ jsonmarket/index.php
@@ -3,35 +3,41 @@ require_once "inc/functions.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-    //  var_dump($_GET);
-    if (isset($_GET['op']) && ($_GET['op'] == "delete") && isset($_GET['id'])) {
-        //echo "Cancello id... " . $_GET['id'];
+    if (isset($_GET['op']) && isset($_GET['id'])) {
 
-        //carico array
-        $myFile = readContentFile($pathData);
-        $strumentiMusicali = json_decode($myFile, true);
-        $exit = false;
+        if ($_GET['op'] === 'buy') {
 
+            echo "compra....";
+        }
 
-        foreach ($strumentiMusicali['items'] as $key => $strumenti) {
-            foreach ($strumenti as $mykey => $strumento) {
-                //   echo $strumento['id'] . ' vs ' . $_GET['id'] . '<br>';
-                if ($strumento['id'] == $_GET['id']) {
-                    // echo "DA CANCELLARE!!!! $key-$mykey <br>";
-                    unset($strumentiMusicali['items'][$key][$mykey]);
-                    $exit = true;
+        if ($_GET['op'] === 'delete') {
+            //echo "Cancello id... " . $_GET['id'];
+            //carico array
+            $myFile = readContentFile($pathData);
+            $strumentiMusicali = json_decode($myFile, true);
+            $exit = false;
+
+            foreach ($strumentiMusicali['items'] as $key => $strumenti) {
+                foreach ($strumenti as $mykey => $strumento) {
+                    //   echo $strumento['id'] . ' vs ' . $_GET['id'] . '<br>';
+                    if ($strumento['id'] == $_GET['id']) {
+                        // echo "DA CANCELLARE!!!! $key-$mykey <br>";
+                        unset($strumentiMusicali['items'][$key][$mykey]);
+                        $exit = true;
+                    }
+                    if ($exit) {
+                        break;
+                    }
                 }
                 if ($exit) {
                     break;
                 }
             }
-            if ($exit) {
-                break;
-            }
+
+            $jsonString = json_encode($strumentiMusicali);
+            $result = writeContentIntoFile($pathData, $jsonString);
         }
 
-        $jsonString = json_encode($strumentiMusicali);
-        $result = writeContentIntoFile($pathData, $jsonString);
     }
 }
 
@@ -68,7 +74,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <body>
     <container>
         <div class="container">
-            <h3>JsonMusicMarket</h3>
+            <h3>
+                <?php echo $shopIcon; ?> JsonMusicMarket
+            </h3>
             <table class="table table-bordered" id="tabella">
                 <thead thead class="thead-dark">
                     <tr>
@@ -90,20 +98,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         $strumentiMusicali = json_decode($myFile, true);
                     }
 
-                    /*
-                    echo "<pre>";
-                    print_r($strumentiMusicali);
-                    echo "</pre>";
-                    */
-
                     $categorie = [];
 
                     foreach ($strumentiMusicali['items'] as $key => $strumenti) {
                         foreach ($strumenti as $strumento) {
 
-                            $delete = "<a href='" . $_SERVER['PHP_SELF'] . "?op=delete&id=" . $strumento['id'] . "' class='btn btn-primary'>Elimina</a>";
+                            $delete = "<a href='" . $_SERVER['PHP_SELF'] . "?op=delete&id=" . $strumento['id'] . "' class='btn btn-primary' onclick='return confirmDelete();'>$deleteIcon</a>";
 
-                            echo '<tr><td>' . $strumento['id'] . '</td><td>' . $strumento['number'] . '</td><td>' . $strumento['marca'] . ' </td><td>' . $strumento['name'] . '</td><td> ' . $strumento['price'] . '</td><td> ' . $key . '</td><td>BUY</td><td>' . $delete . '</td></tr>';
+                            $buy = "<a href='" . $_SERVER['PHP_SELF'] . "?op=buy&id=" . $strumento['id'] . "' class='btn btn-primary'>$cartIcon</a>";
+
+                            echo '<tr><td>' . $strumento['id'] . '</td><td>' . $strumento['number'] . '</td><td>' . strtoupper($strumento['marca']) . ' </td><td>' . $strumento['name'] . '</td><td> ' . $strumento['price'] . '</td><td><strong>' . strtoupper($key) . '</strong></td><td>' . $buy . '</td><td>' . $delete . '</td></tr>';
                             $categorie[] = $key;
 
                         }
@@ -112,35 +116,65 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     ?>
                 </tbody>
             </table>
-            <?php
-            $instruments = array_unique($categorie);
 
-            ?>
-            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                <div class="form-group">
-                    <label for="testo">Qta</label>
-                    <input type="numeric" class="form-control" id="qta" name="qta" placeholder="1" required>
-                    <label for="testo">Marca</label>
-                    <input type="text" class="form-control" id="marca" name="marca" placeholder="marca" required>
-                    <label for="testo">Descrizione</label>
-                    <input type="text" class="form-control" id="description" name="description"
-                        placeholder="description" required>
-                    <label for="testo">Price</label>
-                    <input type="numeric" class="form-control" id="price" name="price" placeholder="price" required>
 
-                    <select id="tipo" name="tipo">
-                        <?php
-                        foreach ($instruments as $key => $value) {
-                            echo '<option value="' . $value . '">' . $value . '</option>';
-                        }
-                        ?>
-                    </select>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <?php
+                            $instruments = array_unique($categorie);
+                            ?>
+                            <h5 class="card-title">Inserisci prodotto</h5>
+                            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                                <div class="form-group">
+                                    <label for="testo">Qta</label>
+                                    <input type="numeric" class="form-control" id="qta" name="qta" placeholder="1"
+                                        required>
+                                    <label for="testo">Marca</label>
+                                    <input type="text" class="form-control" id="marca" name="marca" placeholder="marca"
+                                        required>
+                                    <label for="testo">Descrizione</label>
+                                    <input type="text" class="form-control" id="description" name="description"
+                                        placeholder="description" required>
+                                    <label for="testo">Price</label>
+                                    <input type="numeric" class="form-control" id="price" name="price"
+                                        placeholder="price" required>
 
+                                    <select id="tipo" name="tipo" class="form-control">
+                                        <?php
+                                        foreach ($instruments as $key => $value) {
+                                            echo '<option value="' . $value . '">' . $value . '</option>';
+                                        }
+                                        ?>
+                                    </select>
+
+                                </div>
+                                <button type="submit" class="btn btn-primary">
+                                    <?php echo $addIcon; ?>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Aggiungi prodotto testo</button>
-            </form>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Il tuo carrello</h5>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </container>
+
+    <script>
+        function confirmDelete() {
+            return confirm("Sei sicuro di voler cancellare?");
+        };
+    </script>
+
 </body>
 
 </html>

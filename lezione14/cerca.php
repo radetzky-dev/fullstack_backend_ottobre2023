1129702051525cebeaf9894e58ce818955b0ebdf
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-26 13:56:57 +0200

    ok

--- lezione14/cerca.php
+++ lezione14/cerca.php
@@ -3,24 +3,40 @@ require_once "inc/connection_data.php";
 require_once "inc/functions.php";
 
 
-if (($_SERVER['REQUEST_METHOD'] == 'POST') && (isset($_POST['find']))) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $findWord = trim($_POST['find']);
-    echo "<p>Cerco: " . $findWord . "</p>";
-    if ($findWord != "") {
-        $records = findStudent($findWord, $config);
-        $numResults = count($records);
-        echo "<h4>Risultati trovati: $numResults</h4>";
-        $count = 0;
-        foreach ($records as $record) {
-            $count++;
-            echo $count . ') ', $record['name'] . ' ' . $record['advisor_id'] . '<br>';
+    if (isset($_POST['find'])) {
+        $findWord = trim($_POST['find']);
+        echo "<p>Cerco: " . $findWord . "</p>";
+        if ($findWord != "") {
+            $records = findStudent($findWord, $config);
+            $numResults = count($records);
+            echo "<h4>Risultati trovati: $numResults</h4>";
+            $count = 0;
+            foreach ($records as $record) {
+                $count++;
+                echo $count . ') ', $record['name'] . ' ' . $record['advisor_id'] . '<br>';
+            }
+        } else {
+            echo "Inserisci almeno un carattere per effettuare le ricerca!<br>";
         }
-    } else {
-        echo "Inserisci almeno un carattere per effettuare le ricerca!<br>";
     }
 
-    //TODO insert inserisce con i ? ?
+    if (isset($_POST["operation"]) && ($_POST["operation"] == 'insert')) {
+        $connection = connectDb($config);
+        if ($connection) {
+            $insertQry = "INSERT INTO student (name,advisor_id,class1,class2,class3) VALUES (?,?,?,?,?)";
+            $sth = $connection->prepare($insertQry);
+            $arrayQry[] = $_POST["name"];
+            $arrayQry[] = $_POST["id_advisor"];
+            $arrayQry[] = $_POST["class1"];
+            $arrayQry[] = $_POST["class2"];
+            $arrayQry[] = $_POST["class3"];
+            $sth->execute($arrayQry); //TODO check constraint
+          //  $sth->debugDumpParams(); //debug
+            echo "Inserito con successo!<br>";
+        }
+    }
 }
 
 ?>

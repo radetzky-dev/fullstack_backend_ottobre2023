2d695db86da2438ccc76b162edba9e1fe276e234
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-26 11:07:39 +0200

    risultati

--- lezione14/pdoconnection.php
+++ lezione14/pdoconnection.php
@@ -5,7 +5,31 @@ $connection = connectDb($config);
 
 if ($connection) {
     echo "Connessione avvenuta con successo!<br>";
-    //TODO qui possiamo compiere operazioni sul database
+    //TUTTI I VALORI
+    $sql = "SELECT * FROM student";
+    $sth = $connection->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+    $sth->execute();
+    $records = $sth->fetchAll();
+    foreach ($records as $record) {
+        echo $record['name'] . ' ' . $record['advisor_id'] . '<br>';
+    }
+
+    //SINGOLO VALORE
+    $sql = "SELECT count(*) as TOTALE_STUDENTI FROM student";
+    $sth = $connection->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+    $sth->execute();
+    //un solo risultato
+    $result = $sth->fetch(PDO::FETCH_ASSOC);
+    echo "Totale studenti: " . $result['TOTALE_STUDENTI'] . "<br>";
+
+    //SINGOLA COLONNA
+    $sth = $connection->prepare("SELECT name, advisor_id FROM student");
+    $sth->execute();
+    $result = $sth->fetchColumn();
+    echo "(prima riga, prima colonna) name = $result<br>";
+
+    $result = $sth->fetchColumn(1);
+    echo "(seconda riga, seconda colonna) advisor_id = $result<br>";
 
     $connection = closeDbConnection($connection);
     if (is_null($connection)) {

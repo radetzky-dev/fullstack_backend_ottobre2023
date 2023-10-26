18a633fb861d5cff5a70f239d010be45b513789a
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-26 11:17:23 +0200

    finale

---
+++ lezione14/parametri.php
@@ -0,0 +1,31 @@
+<?php
+require_once "inc/connection_data.php";
+
+$connection = connectDb($config);
+
+if ($connection) {
+    echo "Connessione avvenuta con successo!<br>";
+    //TUTTI I VALORI
+    $sql = "SELECT * FROM student WHERE name LIKE :param1 and advisor_id=:param2";
+
+    $sth = $connection->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+    $sth->execute(array('param1' => '%mm%', 'param2' => 3));
+    $records = $sth->fetchAll();
+    foreach ($records as $record) {
+        echo $record['name'] . ' ' . $record['advisor_id'] . '<br>';
+
+        $sth->execute(['param1' => '%k%', 'param2' => 2]);
+        $records = $sth->fetchAll();
+        foreach ($records as $record) {
+            echo $record['name'] . ' ' . $record['advisor_id'] . '<br>';
+        }
+
+        $connection = closeDbConnection($connection);
+        if (is_null($connection)) {
+            echo "Disconnesso con successo<br>";
+        }
+    }
+} else {
+    echo "Impossibile connettersi al DB!<br>";
+}
+

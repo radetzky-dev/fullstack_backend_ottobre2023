9ea70eed93d240dfaec227979b5075a3312435e4
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-27 12:14:33 +0200

    spinacio

--- lezione15/classi.php
+++ lezione15/classi.php
@@ -7,7 +7,7 @@ class Vegetable
 
     public $name;
 
-    public function __construct($edible, $color = "green", $name ="ortaggio")
+    public function __construct($edible, $color = "green", $name = "ortaggio")
     {
         $this->edible = $edible;
         $this->color = $color;
@@ -17,6 +17,11 @@ class Vegetable
     public function getName()
     {
         return $this->name;
+    }
+
+    public static function getType()
+    {
+        return "Sono un vegetale!";
     }
 
     public function isEdible()
@@ -30,17 +35,26 @@ class Vegetable
     }
 }
 
-class Spinach extends Vegetable {
+class Spinach extends Vegetable
+{
     public $cooked = false;
+
+    public const SAPORE = "AMARO";
 
     public function __construct()
     {
-        parent::__construct(true, "green",'spinacio');
+        parent::__construct(true, "green", 'spinacio');
     }
 
     public function cook()
     {
         $this->cooked = true;
+    }
+
+    //sovrascrivo il metodo
+    public static function getType()
+    {
+        return "Sono uno spinacio!";
     }
 
     public function isCooked()
@@ -49,10 +63,35 @@ class Spinach extends Vegetable {
     }
 }
 
-$cicoria = new Vegetable (true,"verde","cicoria");
-echo 'Sono '.$cicoria->getName() . ' edibile '.$cicoria->isEdible () . ' colore '.$cicoria->getColor () .'<br>';
+echo "Che tipo sono? " . Vegetable::getType() . '<br>';
 
-$spinacio = new Spinach ();
-echo 'Sono '.$spinacio->getName() . ' edibile '.$spinacio->isEdible () . ' colore '.$spinacio->getColor () .' cucinato'.$spinacio->isCooked () .'<br>';
+$cicoria = new Vegetable(true, "verde", "cicoria");
+echo 'Sono ' . $cicoria->getName() . ' edibile ' . $cicoria->isEdible() . ' colore ' . $cicoria->getColor() . '<br>';
+
+echo "Che tipo sono? " . Spinach::getType() . '<br>';
+
+$spinacio = new Spinach();
+echo 'Sono ' . $spinacio->getName() . ' edibile ' . $spinacio->isEdible() . ' colore ' . $spinacio->getColor() . ' cucinato' . $spinacio->isCooked() . '<br>';
 $spinacio->cook();
-echo 'Sono '.$spinacio->getName() . ' edibile '.$spinacio->isEdible () . ' colore '.$spinacio->getColor () .' cucinato'.$spinacio->isCooked () .'<br>';
+echo 'Sono ' . $spinacio->getName() . ' edibile ' . $spinacio->isEdible() . ' colore ' . $spinacio->getColor() . ' cucinato' . $spinacio->isCooked() . '<br>';
+
+echo Spinach::SAPORE;
+echo "<hr>";
+
+if($cicoria instanceof Vegetable) {
+  echo "The object is Vegetable<br>";
+
+}
+
+// The object is also an instance of the class it is derived from
+if($spinacio instanceof Spinach) {
+  echo "The object is Spinach Cucinato?".$spinacio->isCooked()."<br>";
+
+}
+
+if($spinacio instanceof Person) {
+    echo "The object is Vegetable<br>";
+  } else
+  {
+    echo "Non è di tipo Person<br>";
+  }

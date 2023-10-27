9cce7f0c0ec1652c6291fc07cbfc88500c7636ba
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-27 11:01:00 +0200

    bind

--- lezione15/crud.php
+++ lezione15/crud.php
@@ -6,8 +6,21 @@ $connection = connectToDB();
 if ($connection) {
     $stmt = $connection->prepare("insert into classes (room) VALUES (?)");
 
-    $room = "148-78";
+    $room = "158-78";
     $stmt->execute([$room]);
+    echo "Inserimento avvenuto con successo!<br>";
+
+    //BIND dei parametri
+    $stmt = $connection->prepare("insert into student (name,advisor_id,class1,class2,class3) VALUES (?,?,?,?,?)");
+
+    $stmt->bind_param("siiii", $name, $adv_id, $c1, $c2, $c3);
+    $name = "Matteo";
+    $adv_id = 2;
+    $c1 = 2;
+    $c2 = 1;
+    $c3 = 1;
+    $stmt->execute();
+
     echo "Inserimento avvenuto con successo!<br>";
 
     closeConnection($connection);

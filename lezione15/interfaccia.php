eb5845bc589d6b3c3f000d86b7da7044d2e950e0
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-27 12:56:38 +0200

    traits abstract

--- lezione15/interfaccia.php
+++ lezione15/interfaccia.php
@@ -7,10 +7,11 @@ interface Animal
     public function mangia($cibo);
 }
 
-class Quadrupedi
+abstract class Quadrupedi
 {
     public $zampe;
 
+    abstract protected function ali();
     public function __construct($zampe)
     {
         $this->zampe = $zampe;
@@ -54,6 +55,11 @@ class Dog extends Quadrupedi implements Animal
         return "I'm a dog<br>";
     }
 
+    public function ali()
+    {
+        return "Non ho ali!<br>";
+    }
+
 
     public function mangia($cibo)
     {
@@ -73,8 +79,51 @@ echo "Mangio il ".$animal->cosaMangia()."<br>";
 
 $animal1 = new Dog(4);
 $animal1->mangia("gatto");
+echo $animal1->ali();
 $animal1->makeSound();
 echo $animal1->getType();
 echo "Ho " . $animal1->getZampe() . ' zampe!<br>';
 
 echo "Mangio il ".$animal1->cosaMangia()."<br>";
+
+echo "<hr>";
+
+class Base {
+    public function sayHello() {
+        echo 'Hello ';
+    }
+}
+
+trait SayWorld {
+    public function sayHello() {
+        parent::sayHello();
+        echo 'World!';
+    }
+}
+
+
+class MyHelloWorld extends Base {
+    use SayWorld;
+}
+
+$o = new MyHelloWorld();
+$o->sayHello();
+echo "<hr>";
+
+trait Ciao{
+   public function saluta(){
+        echo "ciao<br>";
+    }
+
+    public function boh()
+    {
+        echo "non so cosa dire<br>";
+    }
+}
+class MyHello {
+    use Ciao;
+}
+
+$o1 = new MyHello();
+$o1->saluta();
+$o1->boh();

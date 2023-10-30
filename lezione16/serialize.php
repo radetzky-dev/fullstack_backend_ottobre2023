df8a05a27aaef06409ee6d38010f00d97b7fe6ee
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-30 11:35:56 +0100

    serialize

--- lezione16/serialize.php
+++ lezione16/serialize.php
@@ -25,3 +25,37 @@ var_dump($data);
 echo "<hr>";
 $test = unserialize($data);
 var_dump($test);
+
+echo "<hr>";
+
+class demoSleepWakeup {
+    public $resourceM;
+    public $arrayM;
+
+    public function __construct() {
+        $this->resourceM = fopen("demo.txt", "w");
+        $this->arrayM = array("ciao","paolo"); // Enter code here
+    }
+
+
+    public function __sleep() {
+        return array('arrayM');
+    }
+
+    public function __wakeup() {
+        $this->resourceM = fopen("demo.txt", "w");
+        $this->arrayM= array ("Uno","due");
+    }
+ 
+    
+}
+echo "<hr>";
+echo "DEMO<br>";
+$obj = new demoSleepWakeup();
+var_dump($obj);
+echo "<hr>";
+$serializedStr = serialize($obj);
+var_dump($serializedStr);
+echo "<hr>";
+var_dump(unserialize($serializedStr));
+

f6d88675d3672b971df50aa0db428e290a5f732b
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-31 14:17:42 +0100

    finale

--- linkstats/errorpage.php
+++ linkstats/errorpage.php
@@ -1,2 +1,2 @@
 <p>Nessun link trovato</p>
-<a href="linkdachiamare.php">Vai alla lista</a>
+<a href="linkdachiamare.php">Vai alla lista dei link disponibili.</a>
--- linkstats/linkdachiamare.php
+++ linkstats/linkdachiamare.php
@@ -1,9 +1,7 @@
 <?php
 require_once "inc/connection_data.php";
-require_once 'vendor/autoload.php';
 
-use Carbon\Carbon;
-
+$connection = connectToDB($config);
 ?>
 
 <!DOCTYPE html>
@@ -16,9 +14,31 @@ use Carbon\Carbon;
             <h3>
                 Books </h3>
             <?php
-            printf("sono le: %s", Carbon::now());
-            printf("Right now in Rome is %s", Carbon::now('Europe/Rome'));
+                if (isset($_GET["op"]) && isset($_GET["id"])) {
+                        // var_dump($_POST["title"]);
+                        if ($connection) {
+                            $sql = "DELETE FROM books WHERE id=".$_GET["id"];
+                            //SCRIVERE NEL DB
+                            $result = runQuery($sql, $connection);
+                        }
+                    }
+
+            if (isset($_POST["title"])) {
+                // var_dump($_POST["title"]);
+                if ($connection) {
+                    $sql = "INSERT INTO books (title,booklink,cover) VALUES ('" . $_POST["title"] . "','" . $_POST["link"] . "','" . $_POST["cover"] . "')";
+                    //SCRIVERE NEL DB
+                    $result = runQuery($sql, $connection);
+                }
+            }
+
             ?>
+            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                Title <input type="text" name="title" id="title" required>
+                Amazon link <input type="text" name="link" id="link" required>
+                Cover link<input type="text" name="cover" id="cover">
+                <input type="submit" value="Inserisci">
+            </form>
             <br>
             <a href="stats.php" target="_blank">Vedi statistiche</a>
             <table class="table table-bordered" id="tabella">
@@ -34,15 +54,14 @@ use Carbon\Carbon;
                 </thead>
                 <tbody>
                     <?php
-                    $connection = connectToDB($config);
                     if ($connection) {
-
                         $sql = "SELECT * FROM books";
                         $result = runQuery($sql, $connection);
                         while ($row = $result->fetch_assoc()) {
                             $link = "index.php?id=" . $row["id"];
+                            $delete = "linkdachiamare.php?op=delete&id=". $row["id"];
 
-                            echo '<tr><td>' . $row["id"] . '</td><td>' . $row["title"] . '</td><td>' . $row["booklink"] . '</td><td><img src="' . $row["cover"] . '" title="cover" width=150></td><td>Modify</td><td><a href="' . $link . '" target="_blank">Clicca</a></td></tr>';
+                            echo '<tr><td>' . $row["id"] . '</td><td>' . $row["title"] . '</td><td>' . $row["booklink"] . '</td><td><img src="' . $row["cover"] . '" title="cover" width=150></td><td><a href="' . $delete . '" class="btn btn-primary" onclick="return confirmDelete();">Elimina</a></td><td><a href="' . $link . '" target="_blank">Clicca</a></td></tr>';
                         }
                     }
                     ?>
@@ -57,6 +76,11 @@ use Carbon\Carbon;
         closeConnection($connection);
     }
     ?>
+        <script>
+        function confirmDelete() {
+            return confirm("Sei sicuro di voler cancellare?");
+        };
+    </script>
 </body>
 
 </html>

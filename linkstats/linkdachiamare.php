8c27d4c5854fb660d1403a0fea09a62dab2d7690
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-10-31 10:22:54 +0100

    link carbon

--- linkstats/linkdachiamare.php
+++ linkstats/linkdachiamare.php
@@ -1,5 +1,9 @@
 <?php
 require_once "inc/connection_data.php";
+require_once 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +14,12 @@ require_once "inc/connection_data.php";
     <container>
         <div class="container">
             <h3>
-                Books
-            </h3>
+                Books </h3>
+            <?php
+            printf("sono le: %s", Carbon::now());
+            printf("Right now in Rome is %s", Carbon::now('Europe/Rome'));
+            ?>
+            <br>
             <a href="stats.php" target="_blank">Vedi statistiche</a>
             <table class="table table-bordered" id="tabella">
                 <thead thead class="thead-dark">
@@ -28,13 +36,13 @@ require_once "inc/connection_data.php";
                     <?php
                     $connection = connectToDB($config);
                     if ($connection) {
-    
+
                         $sql = "SELECT * FROM books";
                         $result = runQuery($sql, $connection);
                         while ($row = $result->fetch_assoc()) {
-                            $link ="index.php?id=".$row["id"];
+                            $link = "index.php?id=" . $row["id"];
 
-                            echo '<tr><td>' . $row["id"] . '</td><td>' . $row["title"] . '</td><td>' . $row["booklink"] . '</td><td><img src="' . $row["cover"] . '" title="cover" width=150></td><td>Modify</td><td><a href="'.$link.'" target="_blank">Clicca</a></td></tr>';
+                            echo '<tr><td>' . $row["id"] . '</td><td>' . $row["title"] . '</td><td>' . $row["booklink"] . '</td><td><img src="' . $row["cover"] . '" title="cover" width=150></td><td>Modify</td><td><a href="' . $link . '" target="_blank">Clicca</a></td></tr>';
                         }
                     }
                     ?>

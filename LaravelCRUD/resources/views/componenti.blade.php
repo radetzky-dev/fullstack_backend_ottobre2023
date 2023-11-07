101744a74a1139a75861923dd64face48775ab7d
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-11-07 12:30:41 +0100

    slot

--- LaravelCRUD/resources/views/componenti.blade.php
+++ LaravelCRUD/resources/views/componenti.blade.php
@@ -10,7 +10,14 @@
     @php
         $message = 'mondo';
         $hello = 'BuonAserA';
+        $slot = 'slot di prova';
     @endphp
 
     NOTA: Usiamo saluto-comp qui mentre nella classe e nel componente si usa salutoComp (camelCase)<br>
     <x-alert type="error" :saluto-comp="$hello" :message="$message" />
+
+
+    <hr>
+    <x-prova>
+        {{ $slot }}
+    </x-prova>
---
+++ LaravelCRUD/resources/views/components/prova.blade.php
@@ -0,0 +1,5 @@
+<div class="alert alert-danger">
+    <h3>Titolo sopra</h3>
+    <p>{{ $slot }}</p>
+    <h4>Titolo sotto</h4>
+</div>

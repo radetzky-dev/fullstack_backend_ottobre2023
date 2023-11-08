2ea90fbd22000d93b73034118dde7e89254d0640
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-11-08 13:32:41 +0100

    controller log

--- example-app/app/Http/Controllers/Controller.php
+++ example-app/app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Services\HelloService;
 use Illuminate\Support\Facades\App;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\Log;
 
 class Controller extends BaseController
 {
@@ -21,14 +22,40 @@ class Controller extends BaseController
 
     public function testRoute()
     {
+        $message = "creo una riga nel log";
+        Log::debug($message);
         echo "<b>Test</b> dal controller!";
+        Log::info("Caricata rotta testRoute");
+
+
+
+        $myNumber = 10;
+        $division = 0;
+
+        try {
+            if ($division == 0) {
+                throw new \Exception("Division by zero");
+            }
+
+            $result = $myNumber / $division;
+            Log::debug($result);
+            echo "Risultato $result<br>";
+        } catch (\Exception $e) {
+            Log::error("Ho gestito l'errore! Errore descrizione: " . $e->getMessage());
+        }
+
+
+
+
+        echo "<br>Operazione conclusa<br>";
+
     }
 
     public function saluta(Request $request)
     {
         echo $this->cheers->saluta($request->input("name")) . '<br>';
         echo $this->cheers->sayHello($request->input("name")) . '<br>';
-        echo $this->cheers->sayBonjour($request->input("name")).'<br>';
+        echo $this->cheers->sayBonjour($request->input("name")) . '<br>';
         echo $this->salutaConHelloService($request->input("name"));
         echo $this->altroSaluto($request->input("name"));
 

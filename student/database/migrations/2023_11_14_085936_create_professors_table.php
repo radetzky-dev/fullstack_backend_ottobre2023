88b15744d99713690403df05036d95874dc69906
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-11-14 10:10:55 +0100

    professor

---
+++ student/database/migrations/2023_11_14_085936_create_professors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('professors', function (Blueprint $table) {
+            $table->id();
+            $table->string("Name", 50);
+            $table->string("Subject", 50);
+            $table->integer("Hours")->nullable();
+            $table->char("Room", 4)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('professors');
+    }
+};

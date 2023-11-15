41626c9c61165e828aed5c3058314971b0b703c1
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-11-15 11:17:08 +0100

    test

--- blog/tests/Unit/ExampleTest.php
+++ blog/tests/Unit/ExampleTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+//use PHPUnit\Framework\TestCase;
+
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
@@ -12,5 +14,20 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+
+    public function test_the_application_returns_a_bad_response(): void
+    {
+        $response = $this->get('/noexists');
+
+        $response->assertStatus(404);
     }
 }

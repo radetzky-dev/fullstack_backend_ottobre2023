e4238f520839e083cb99dcb605d43681fed38ce6
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-11-16 10:00:36 +0100

    files

--- cart/app/Http/Controllers/ProductController.php
+++ cart/app/Http/Controllers/ProductController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function productList()
+    {
+        $products = Product::all();
+
+        return view('products', compact('products'));
+    }
 }
